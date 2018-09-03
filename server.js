24f31de74d7de361bdbab6e8e3d3f8d911c53e56
mistermappy123 <mikhal.gelo@gmail.com>
2018-09-02 23:04:41 -0700

    heroku changes

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ var router = express.Router();
 var passport = require('passport');
 var session = require('express-session');
 var multer = require('multer'); 
-<<<<<<< HEAD
-var userPhotos = require('./model/photo');
-=======
->>>>>>> 1cc1544f1704ac1023cf1d8c214412c9883e5a17
 var aws = require('aws-sdk');
 var multers3 = require('multer-s3');
 var imager = require('multer-imager');
@@ -28,7 +24,6 @@ var upload = multer({
 });
 
 const Sequelize = require('sequelize');
-<<<<<<< HEAD
 /*const sequelize = new Sequelize('users', 'postgres', 'nppsjuoll', {
     host: 'localhost',
     dialect: 'postgres'
@@ -38,16 +33,6 @@ const sequelize = new Sequelize('icloneDatabase', 'mistermappy123', 'nppsjuoll',
     port: 5432,
     dialect: 'postgres'
 });
-=======
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-        ssl: true
-    }
-})
-//const sequelize = new Sequelize('postgres://qqfapouvhewocy:c4a6920ceb8cd92f47466624c28136032fbca36e2c80a7e021a3068285886116@ec2-107-22-192-11.compute-1.amazonaws.com:5432/d2rp83vuathmlu');
->>>>>>> 1cc1544f1704ac1023cf1d8c214412c9883e5a17
 
 sequelize
   .authenticate()
@@ -156,11 +141,7 @@ router.get('/loginerror', (req, res) => {
     res.render('loginerror');
 })
 
-<<<<<<< HEAD
 router.post('/login', function(req, res, next) {
-=======
-/*router.post('/login', function(req, res, next) {
->>>>>>> 1cc1544f1704ac1023cf1d8c214412c9883e5a17
     passport.authenticate('local-login', function(err, user, info) {
       if (err) { 
           return next(err); 
@@ -169,11 +150,11 @@ router.post('/login', function(req, res, next) {
           return res.redirect('/loginerror'); 
       };
       console.log(user.username); 
-      userName = user.username;
+      userName = user.username; 
       return res.redirect('/home');
 
     })(req, res, next);
-  })*/
+  })
 
 router.get('/faq', (req, res) => {
     res.render('faq')
