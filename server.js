a61ef5b33f109a0158443479320a594b5ad65780
mistermappy123 <mikhal.gelo@gmail.com>
2018-09-27 11:44:27 -0700

    deleted Aws credentials

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let passport = require('passport');
 let multer = require('multer'); 
 let aws = require('aws-sdk');
 let multers3 = require('multer-s3');
-aws.config.loadFromPath('./S3/config.json');
+//aws.config.loadFromPath('./S3/config.json');
 let s3 = new aws.S3();  
 let fs = require('fs');
 let SequelizeStore = require('connect-session-sequelize')(session.Store); 
@@ -292,91 +292,5 @@ router.post('/delete', (req, res) => {
         res.redirect('/profile')
     });
 });
-
-var LocalStrategy = require('passport-local').Strategy; 
-var bcrypt = require('bcrypt');
-var User = require('./models').Users
-var saltRounds = 10;
-
-//module.exports = function(passport){ 
-
-	function getUserParams(req) {
-    var body = req.body
-    return {
-        id: body.id,
-        username: body.username,
-        password: body.password
-    	};
-	}
-
-	passport.use('local-signup', new LocalStrategy({
-		username: 'username',
-		password: 'password',
-		passReqToCallback: true
-	}, processSignupCallback));
-
-	passport.use('local-login', new LocalStrategy({
-		username: 'username',
-		password: 'password'
-	}, processLoginCallback))
-
-	function processSignupCallback(req, username, password, done) {
-		User.findOne({
-			where: {
-				'username': username
-			},
-			attributes : ['id']
-		})
-		.then(function(user){
-			if(user){
-				return done(null, false);
-			} else {
-
-				var userToCreate = getUserParams(req);
-				
-				bcrypt.hash(userToCreate.password, saltRounds, function(err, hash){
-				userToCreate.password = hash; 
-				User.create(userToCreate)
-				.then(function(createdRecord){
-					createdRecord.password = undefined;
-					return done(null, createdRecord);
-					})})
-			}})}
-
-	function processLoginCallback(username, password, done){
-		User.findOne({
-			where: {
-				'username': username
-			}
-		})
-		.then(function(user){
-			if (!user) {
-				return done(null, false)
-			}
-			else {
-			bcrypt.compare(password, user.password, function(err, res){
-				if (err) {
-					return done(err)
-				}
-				if (!res){
-					return done(null, false)
-				}
-				return done(null, user);
-			})}
-		});
-	}
-//}	
-passport.serializeUser(function(user, done){
-    done(null, user)
-});
-
-/*passport.deserializeUser(function(id, done){
-    User.findById(id, function(err, user){
-        done(err, user);
-    })	
-});*/
-passport.deserializeUser(function(user, done){
-    done(null, user); 
-})
 
 module.exports = router; 
